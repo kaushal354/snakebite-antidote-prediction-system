84db337f27b3e34e4b2772e027adf75000cb01b5
Kaushal prasad <111481878+kaushal354@users.noreply.github.com>
2025-11-28 17:18:57 +0530

    Update asgi.py

--- snakebite/asgi.py
+++ snakebite/asgi.py
@@ -9,8 +9,6 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 
 import os
 
-import home.sqlite_patch
-
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'snakebite.settings')
