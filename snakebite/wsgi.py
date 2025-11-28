60af8ef18f9b8760e6bb5195317ba0f33b6ee8f1
Kaushal prasad <111481878+kaushal354@users.noreply.github.com>
2025-11-28 15:56:04 +0530

    Update wsgi.py

--- snakebite/wsgi.py
+++ snakebite/wsgi.py
@@ -7,6 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
+import home.sqlite_patch
+
+
 import os
 
 from django.core.wsgi import get_wsgi_application
